0f29a586cef44d99e2dd6a41cea0647d42b500de
riddlet <riddlet@STU16-03.catlin.edu>
2014-01-18 15:32:47 -0800

    Added some enemies in the main thing

--- src/plat/mainGraphics/Platformer.java
+++ src/plat/mainGraphics/Platformer.java
@@ -1,7 +1,11 @@
 package plat.mainGraphics;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Random;
 
+import Basic_Enemy.Basic_Enemy;
+import plat.interfaces.EntityInterface;
 import janus.engine.SimpleGraphics;
 import janus.engine.pens.SimplestPen;
 
@@ -11,22 +15,41 @@ public class Platformer extends SimpleGraphics{
 		super(800, 600, "Plat");
 	}
 	
+	Random rand = new Random();
+	
+	ArrayList<EntityInterface> enemy_list;
 	
 
 	@Override
 	public void start(SimplestPen pen) {
 		level=1;
+		
+		for (int i = 0; i<10; i++){
+			int vx = rand.nextInt(800);
+			int vy = rand.nextInt(600);
+			
+			enemy_list.add(new Basic_Enemy(pen, vx, vy));
+			
+		}
+		
+		enemy_list = new ArrayList<EntityInterface>();
 	}
 
 	@Override
 	public void update(SimplestPen pen) {
-		// TODO Auto-generated method stub
+		
+		for (EntityInterface e: enemy_list){
+			e.update(pen);
+		}
 		
 	}
 	@Override
 	public void draw(SimplestPen pen) {
 		pen.setColor(Color.RED);
 		pen.drawCircle(100, 100, 50);
+		
+		for (EntityInterface e: enemy_list){
+			e.draw(pen);
 		
 	}
 
