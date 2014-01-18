4c5c1b17c1e8e9e7f7e3351c4f12274b2bbf1bc0
riddlet <riddlet@STU16-03.catlin.edu>
2014-01-18 15:01:59 -0800

    Added basic player interface

---
+++ src/plat/player/PlayerInterface.java
@@ -0,0 +1,14 @@
+package plat.player;
+
+import janus.engine.pens.SimplestPen;
+import plat.physics.Point;
+import plat.physics.Vector;
+
+public interface PlayerInterface {
+	
+	public void left_key_press();
+	public void right_key_press();
+	public void up_key_press();
+	public void down_key_pess();
+
+}
