e5ebe23b11feb13185561ff008697eacebc4d7b2
Gregor Peach <gregorpeach@gmail.com>
2014-01-18 15:29:42 -0800

    Block interface update.\

--- src/plat/interfaces/BlockInterface.java
+++ src/plat/interfaces/BlockInterface.java
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 package plat.interfaces;
 
-=======
-<<<<<<< HEAD
-import plat.physics;
+import plat.physics.Point;
 
-=======
-package plat.interfaces;
->>>>>>> 5cd236da4f3828d16a55b56b90344ef2ab5044b3
->>>>>>> branch 'master' of https://github.com/Others/CodeDayPlatform.git
 public interface BlockInterface {
   public Point location ();
   public int type ();
