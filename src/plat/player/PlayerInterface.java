2762b342acfd6fa09aae79f80a92baff53aecd3f
riddlet <riddlet@STU16-03.catlin.edu>
2014-01-18 15:05:00 -0800

    fixed basic stuff in plyaer interface

--- src/plat/player/PlayerInterface.java
+++ src/plat/player/PlayerInterface.java
@@ -1,8 +1,5 @@
 package plat.player;
 
-import janus.engine.pens.SimplestPen;
-import plat.physics.Point;
-import plat.physics.Vector;
 
 public interface PlayerInterface {
 	
@@ -11,4 +8,5 @@ public interface PlayerInterface {
 	public void up_key_press();
 	public void down_key_pess();
 
+	
 }
