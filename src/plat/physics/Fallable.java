8bf875f7fc3a2e67779625e5d415da55ac51ae22
Gregor Peach <gregorpeach@gmail.com>
2014-01-18 18:53:49 -0800

    Test

--- src/plat/physics/Fallable.java
+++ src/plat/physics/Fallable.java
@@ -6,14 +6,17 @@ public abstract class Fallable {
 	public abstract int bottom();
 	public abstract int height();
 	public abstract Vector getVector();
+	public boolean falling=false;
 	public Vector gravitate(Hitable[] h){
 		Vector v=this.getVector();
 		for(Hitable e:h){
 			if(this.bottom()>=e.top() && !(this.getLowerRight()<e.getTopLeft() || this.getLowerLeft()>e.getTopRight())){
 				v.vertical=0;
+				falling=false;
 				return v;
 			}
 		}
+		falling=true;
 		v.add(new Vector(0,.01));
 		return v;
 	}
