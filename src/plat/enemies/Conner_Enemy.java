8c386fe3ffa629b2075bee332333697c99f243d7
Conner Hansen <hansenc@catlin.edu>
2014-01-18 17:16:56 -0800

    Working on enemy.

--- src/plat/enemies/Conner_Enemy.java
+++ src/plat/enemies/Conner_Enemy.java
@@ -1,13 +1,15 @@
-<<<<<<< HEAD
 package plat.enemies;
 
 import plat.interfaces.*;
+import plat.physics.*;
 
 class Conner_Enemy implements EntityInterface {
-	Conner_Enemy (xStart, yStart) {
+	Point position;
+	Vector velocity;
+	Conner_Enemy (int xStart, int yStart) {
+		position = new Point (xStart, yStart);
+	}
+	public update (SimplestPen pen) {
 		
 	}
 }
-=======
-package plat.enemies;
->>>>>>> branch 'master' of https://github.com/Others/CodeDayPlatform.git
