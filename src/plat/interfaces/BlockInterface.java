8d7940c95d24b8e10973012c3ad35f6ef39d319f
Gregor Peach <gregorpeach@gmail.com>
2014-01-18 16:50:41 -0800

    Clean

--- src/plat/interfaces/BlockInterface.java
+++ src/plat/interfaces/BlockInterface.java
@@ -3,13 +3,6 @@ package plat.interfaces;
 import plat.physics.Point;
 
 public interface BlockInterface {
-<<<<<<< HEAD
 	public Point location();
-
 	public int type();
 }
-=======
-  public Point location ();
-  public int type ();
-}
->>>>>>> branch 'master' of https://github.com/Others/CodeDayPlatform.git
